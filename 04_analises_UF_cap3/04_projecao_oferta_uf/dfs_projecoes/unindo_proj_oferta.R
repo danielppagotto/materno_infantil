7fbfcc759a04ea86ddd9fa91c256fd56c3a982f4
Daniel Pagotto <danielppagotto@gmail.com>
2025-05-10 11:08:49 -0300

    update

---
+++ 04_analises_UF_cap3/04_projecao_oferta_uf/dfs_projecoes/unindo_proj_oferta.R
@@ -0,0 +1,37 @@
+
+library(tidyverse)
+
+setwd("~/GitHub/materno_infantil/04_analises_UF_cap3/04_projecao_oferta_uf/dfs_projecoes")
+
+# Carregar pacotes necessários
+library(dplyr)
+library(readr)
+library(purrr)
+
+
+# Listar todos os arquivos CSV na pasta
+arquivos_csv <- list.files(pattern = ".csv$")
+
+# Função para ler cada arquivo e adicionar uma coluna com o nome do arquivo
+ler_arquivo <- function(arquivo) {
+  # Extrair o prefixo do nome do arquivo (AM, GO, MS, RR)
+  prefixo <- sub("_projecao\\.csv$", "", arquivo)
+  
+  # Ler o arquivo CSV
+  dados <- read_csv(arquivo)
+  
+  # Adicionar uma coluna com o identificador do arquivo
+  dados$origem <- prefixo
+  
+  return(dados)
+}
+
+# Aplicar a função a cada arquivo e combinar em um único dataframe
+dados_combinados <- arquivos_csv %>%
+  map_dfr(ler_arquivo)
+
+# Visualizar as primeiras linhas do dataframe combinado
+head(dados_combinados)
+
+# Salvar o dataframe combinado em um novo arquivo CSV
+write_csv(dados_combinados, "~/GitHub/materno_infantil/04_analises_UF_cap3/04_projecao_oferta_uf/dfs_projecoes/oferta_proj_UF.csv")
