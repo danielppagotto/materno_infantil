2e68ee614ece57a652cf3cce25f3cb5adce56544
Daniel Pagotto <danielppagotto@gmail.com>
2025-05-18 13:04:53 -0300

    Update 06_2_funcao_calculo2332.R

--- 02_1_script_capitulo3/06_2_funcao_calculo2332.R
+++ 02_1_script_capitulo3/06_2_funcao_calculo2332.R
@@ -5,9 +5,74 @@ library(tidyverse)
 
 # Demanda -----------------------------------------------------------------
 
-servicos19_32 <- 
-  vroom::vroom("~/GitHub/materno_infantil/02_script/04_servicos/servicos19_32.csv") |> 
-  select(-`...1`)
+dremio_host <- Sys.getenv("endereco")
+dremio_port <- Sys.getenv("port")
+dremio_uid <- Sys.getenv("uid")
+dremio_pwd <- Sys.getenv("datalake")
+
+
+channel <- odbcDriverConnect(sprintf("DRIVER=Dremio Connector;
+                                     HOST=%s;
+                                     PORT=%s;
+                                     UID=%s;
+                                     PWD=%s;
+                                     AUTHENTICATIONTYPE=Basic Authentication;
+                                     CONNECTIONTYPE=Direct", 
+                                     dremio_host, 
+                                     dremio_port, 
+                                     dremio_uid, 
+                                     dremio_pwd))
+
+query <- 'SELECT * FROM "@daniel"."servicos_NV_24_30"'
+
+servicos2432 <- sqlQuery(channel, 
+                     query, 
+                     as.is = TRUE) 
+
+
+servicos24_32_tratado <- 
+  servicos2432 |> 
+  rename(mes_proc_rea = mês_procedimento_realizado,
+         qtd_proc = quantidade, 
+         qtd_nasc = qtd,
+         cod_regsaude = cod_regsaud,
+         publico = Público,
+         mes_programado = mes) |> 
+  mutate(mes_programado = as.numeric(mes_programado)) |> 
+  mutate(ano_proc_rea = 
+           year(mes_proc_rea)) |> 
+  filter(nivel_atencao == "APS" & 
+           publico != "Gestantes de Alto Risco") |>
+  filter(tipo_procedimento == "Ações Educacionais" |
+           tipo_procedimento == "Consultas ou Visitas") |> 
+  filter(mes_programado < 35) |> 
+  filter(procedimento != "Avaliação odontológica") |> 
+  filter(procedimento != "Visita domiciliar") |> 
+  mutate(cod_regsaude = as.numeric(cod_regsaude)) |> 
+  filter(ano_proc_rea > 2023) |> 
+  mutate(mes_proc_rea = as.Date(mes_proc_rea))
+
+# verificando visualmente
+
+servicos24_32_tratado |> 
+  filter(cod_regsaude == "11001") |> 
+  filter(procedimento == "Consulta pré-natal") |> 
+  group_by(mes_proc_rea) |> 
+  summarise(qtd_proc = sum(qtd_proc)) |> 
+  ungroup() |> 
+  ggplot(aes(x = mes_proc_rea, 
+             y = qtd_proc)) +
+  geom_line() + 
+  theme(axis.text.x = element_text(angle = 90, 
+                                   hjust = 1, 
+                                   vjust = 0.5)) + 
+  ylim(0, 3000) +
+  theme_bw() +
+  xlab("Mês/ano") + 
+  ylab("Quantidade de procedimentos")
+
+
+
 
 # Carregando dados
 
@@ -31,56 +96,11 @@ foco_clinico <-
 
 # este bloco é usado para selecionar as observações dos melhores modelos de projeção 
 
-melhor_modelo <- 
-  read.csv("~/GitHub/materno_infantil/02_script/03_outputs_projecoes/prophet/metricas_previsoes_prophet/melhor_modelo.csv") |> 
-  mutate(cod_regsaud = 
-           as.character(cod_regsaud)) |> 
-  mutate(reg_concat = 
-                      paste0(cod_regsaud, 
-                             .model_desc))
-
-vetor <- melhor_modelo$reg_concat
-
-servicos23 <- servicos19_32 |> 
-                filter(mes_proc_rea < "2024-01-01") |> 
-                mutate(reg_concat = "dados reais")
-
-# Abaixo filtramos os modelos com melhores resultados 
-
-servicos_demais <- servicos19_32 |> 
-                    filter(mes_proc_rea >= "2019-01-01") |> 
-                    mutate(cod_regsaude = 
-                             as.character(cod_regsaude)) |> 
-                    mutate(reg_concat = 
-                             paste0(cod_regsaude,
-                                    .model_desc)) |> 
-                    filter(reg_concat %in% vetor)
-
-servicos <- rbind(servicos23, 
-                  servicos_demais) |> 
-            mutate(ano_proc_rea = 
-                     year(mes_proc_rea)) |> 
-            filter(nivel_atencao == "APS" & 
-                     publico != "Gestantes de Alto Risco") |>
-            filter(tipo_procedimento == "Ações Educacionais" |
-                   tipo_procedimento == "Consultas ou Visitas") |> 
-            filter(mes_programado < 35) |> 
-            filter(procedimento != "Avaliação odontológica") |> 
-            filter(procedimento != "Visita domiciliar") |> 
-            mutate(cod_regsaude = as.numeric(cod_regsaude)) |> 
-            left_join(cobertura, 
-                      by = c("cod_regsaude"="cod_regsaud"))
 
 write.csv(servicos, 
             "~/GitHub/materno_infantil/02_1_script_capitulo3/06_servicos/servicos19_32_tratado.csv")
 
 
-servicos |> 
-  filter(cod_regsaude == "11001") |> 
-  filter(procedimento == "Consulta pré-natal") |> 
-  ggplot(aes(x = mes_proc_rea, 
-             y = qtd_proc)) + geom_smooth(method = "loess")
-  
 
 # Oferta ------------------------------------------------------------------
 
